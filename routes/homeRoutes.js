6ea967f0caca7d0850b8937743c69e55dd111729
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-22 14:33:17 +0100

    Updated homepage

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const Project = require('../models/Project');
 
-router.get('/', (req, res) => {
-    res.send('home')
+router.get('/', async (req, res) => {
+    try {
+        const projectData = await Project.findAll();
+        const plainData = projectData.map((project) => project.get({ plain: true }));
+        res.render('home', {
+            plainData
+        })
+    } catch (err) {
+        res.status(403).json(err)
+    }
 });
 
 module.exports = router;
